a6d30bc6e2ad3bf976638d54e497922a48987c9d
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-11-20 16:53:45 +0100

    5hz

--- demo_slidingwindow.py
+++ demo_slidingwindow.py
@@ -6,7 +6,7 @@ import matplotlib.gridspec as gridspec
 RATE = 16000
 CHANNELS = 2
 FORMAT = 'S16_LE'
-CHUNK = RATE // 10  # read 0.5 second per read
+CHUNK = RATE // 5
 WINDOW_SEC = 2
 WINDOW_SIZE = RATE * WINDOW_SEC
 
@@ -74,4 +74,4 @@ while True:
     ax_fft.set_title(f'Hydrophone FFT (last {WINDOW_SEC} sec)')
     ax_fft.set_ylim(0, 100000)
 
-    plt.pause(0.01)
+    plt.pause(0.1)
