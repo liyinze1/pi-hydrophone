4114594684930b3a764e24a1c7724429083a5103
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-12-17 17:41:13 +0100

    update

--- uart_control.py
+++ uart_control.py
@@ -22,7 +22,7 @@ class UART_control:
             b = ser.read(1)
             if not b:
                 continue
-            if b != b's':
+            if b != b'|':
                 continue
             nid = ser.read(1)
             data = ser.read(4)
